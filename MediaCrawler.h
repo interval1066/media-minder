03e09ef4c3f768bb3eb5cc7f8a91519daa2f30b2
harryevansesq <harryevansesq@gmail.com>
2025-03-24 18:13:19 -0700

    Removed files not needed to build.

--- MediaCrawler.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <filesystem>
-#include <unordered_set>
-#include <string>
-
-namespace fs = std::filesystem;
-
-void scanDirectory(const fs::path&, const std::unordered_set<std::string>&);
